30d0d58a25a38946a21dff4553afa0cad0744193
k2wl <kar2wlan@gmail.com>
2016-07-16 13:05:32 +0530

    Update dex2oat / ART flags to decrease the errors

--- product/system_prop.mk
+++ product/system_prop.mk
@@ -98,4 +98,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-flags=--no-watch-dog
+	dalvik.vm.dex2oat-Xms=8m \
+	dalvik.vm.dex2oat-Xmx=96m \
+	dalvik.vm.dex2oat-flags=--no-watch-dog \
+	dalvik.vm.dex2oat-filter=speed \
+	dalvik.vm.image-dex2oat-Xms=48m \
+	dalvik.vm.image-dex2oat-Xmx=48m \
+    dalvik.vm.image-dex2oat-filter=everything \
