d1a9e83abdc11f5699e47762f4676795c5cd5088
sub77 <sub77@ymail.com>
2016-08-10 10:42:49 +0000

    Update platform

--- board/platform.mk
+++ board/platform.mk
@@ -3,13 +3,20 @@ BOARD_VENDOR := samsung
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8226
-TARGET_CPU_VARIANT := krait
 
-# Architecture
 TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := krait
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a7
+
+ENABLE_CPUSETS := true
 
 # Bootloader
 TARGET_NO_BOOTLOADER := true
