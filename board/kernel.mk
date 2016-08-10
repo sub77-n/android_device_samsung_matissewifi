3c27f094f314220710a907a37a4950d4c25189e9
sub77 <sub77@ymail.com>
2016-08-10 12:25:34 +0200

    Update kernel

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,13 +1,13 @@
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=22 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom vmalloc=400M user_debug=23 msm_rtb.filter=0x37 androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-BOARD_KERNEL_BASE := 0x80200000
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
 BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_HEADER_ARCH := arm
-TARGET_KERNEL_SOURCE := kernel/samsung/msm8930-common
-TARGET_KERNEL_CONFIG := cyanogen_serrano_defconfig
-TARGET_KERNEL_VARIANT_CONFIG := msm8930_serrano_eur_3g_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8226
+TARGET_KERNEL_CONFIG := du_matissewifi_defconfig
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+BOARD_KERNEL_SEPARATED_DT := true
